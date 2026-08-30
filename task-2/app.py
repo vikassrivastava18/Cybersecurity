053ea5e6430ea5ca8e495583fb1cf4c420084711
vikassrivastava18 <vikassrinitb@gmail.com>
2026-08-31 00:29:32 +0530

    Add Flask limiter

--- task-2/app.py
+++ task-2/app.py
@@ -1,7 +1,7 @@
-import logging
-
 from flask import Flask, render_template, url_for, redirect, request
-from flask_login import login_user, logout_user, login_required, LoginManager
+from flask_login import login_user, logout_user, login_required, LoginManager, current_user
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from models import User, db
@@ -27,11 +27,17 @@ with app.app_context():
 def login():
     return render_template('login.html')
 
+limiter = Limiter(
+    get_remote_address,
+    app=app
+)
+
+
 @app.route('/login', methods=['POST'])
+@limiter.limit("5 per minute")
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
 
     user = User.query.filter_by(email=email).first()
 
@@ -39,18 +45,47 @@ def login_post():
         return redirect(url_for('login'))
 
     login_user(user, remember=remember)
+    print("Login successful")
     return redirect(url_for('profile'))
 
-@app.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again  
+        return redirect(url_for('signup'))
+
+    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('login'))
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
